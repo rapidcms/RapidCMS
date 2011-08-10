16e123ce0e75ba1911aa7edcfee4336ba34f98e0
unknown <Jaime@.(none)>
2011-08-10 02:40:23 -0700

    Fixed injection exploit in user class

--- rapid/includes/class.user.php
+++ rapid/includes/class.user.php
@@ -1,6 +1,8 @@
 <?php
 class user
 {
+	private $db;
+
 	public $id;
 	public $username;
 	public $password;
@@ -8,57 +10,56 @@ class user
 	public $role;
 	public $error;
 	
-	public function __construct()
-	{
+	public function __construct() {
+		$this->connect();
+	}
+
+	private function connect() {
+		$this->db = new mysqli(DBHOST, DBUSER, DBPASS, DBNAME);
+	}
+	private function sanitize($dirty) {
+		return $this->db->real_escape_string($dirty);
 	}
 	
-	public function load($name=false)
-	{
-		if ($name) 
-		{
-			$sql = "SELECT * FROM users where name='" . $this->username . "';";
+	public function load($name=false) {
+
+		if ($name) {
+			$sql = "SELECT * FROM users where name='" . $this->sanitize($this->username) . "';";
+		} else	{
+			$sql = "SELECT * FROM users where id='" . $this->sanitize($this->id) . "';";
 		}
-		else
-		{
-			$sql = "SELECT * FROM users where id='" . $this->id . "';";
-		}
-		$db = new mysqli(DBHOST, DBUSER, DBPASS, DBNAME);
-		$result = $db->query($sql);
-		if ($result->num_rows > 0)
-		{	
-			
+
+		$result = $this->db->query($sql);		
+
+		if ($result->num_rows > 0) {	
 			$row = $result->fetch_object();
 			$this->id = $row->id;
 			$this->role = $row->role;
 			$this->username = $row->name;
 			$this->password = $row->pass;
 			return true;
-		}
-		else
-		{
+		} else {
 			$this->error[] = "Invalid username or password";
 			return false;
 		}
 	}
 	
-	public function authenticate()
-	{
-		if ($this->username == "")
-		{
+	public function authenticate() {
+		$this->db = new mysqli(DBHOST, DBUSER, DBPASS, DBNAME);
+
+		if ($this->username == "") {
 			$this->error[] = "Cannot log in: Username cannot be blank.";
 			return false;
 		}
 		
-		if ($this->unencrypted_password == "")
-		{
+		if ($this->unencrypted_password == "") {
 			$this->error[] = "Cannot log in: Password cannot be blank.";
 			return false;
 		}
-		$db = new mysqli(DBHOST, DBUSER, DBPASS, DBNAME);
-		$result = $db->query("SELECT * FROM users where name='" . $this->username . "' AND pass='" . md5($this->unencrypted_password) . "';");
-		if ($result->num_rows > 0)
-		{	
-			
+
+		$result = $this->db->query("SELECT * FROM users where name='" . $this->sanitize($this->username) . "' AND pass='" . md5($this->sanitize($this->unencrypted_password)) . "';");
+
+		if ($result->num_rows > 0) {	
 			$row = $result->fetch_object();
 			$this->id = $row->id;
 			$this->username = $row->name;
@@ -66,77 +67,58 @@ class user
 			$this->role = $row->role;
 			
 			return true;
-		}
-		else
-		{
+		} else {
 			$this->error[] = "Invalid username or password";
 			return false;
 		}
 	}
 	
-	public function save()
-	{
-		if ($this->username == "")
-		{
+	public function save() {
+		if ($this->username == "") {
 			$this->error[] = "Cannot create user: no user specified";
 			return false;
 		}
 		
-		if ($this->unencrypted_password == "")
-		{
+		if ($this->unencrypted_password == "") {
 			// it's ok if the password is not here if we are updating a user
 			// but if the user isn't being updated, then we throw an error
-			if (!isset($this->id))
-			{
+			if (!isset($this->id)) {
 				$this->error[] = "Cannot create user: no password specified";
 				return false;
 			}
 		}
 		
-		if (!isset($this->role))
-		{
+		if (!isset($this->role)) {
 			$this->role = "user";
 		}
 		
-		if (isset($this->id)) 
-		{
-			$sql  = "UPDATE users SET name='" . $this->username . "'";
-			if (strlen($this->unencrypted_password) > 0)
-			{
-				$sql .= ", pass='" . md5($this->unencrypted_password) . "'";
+		if (isset($this->id)) {
+			$sql  = "UPDATE users SET name='" . $this->sanitize($this->username) . "'";
+			if (strlen($this->unencrypted_password) > 0) {
+				$sql .= ", pass='" . md5($this->sanitize($this->unencrypted_password)) . "'";
 			}
 			$sql .= ", role='" . $this->role . "'";
 			$sql .= " WHERE id=" . $this->id . ";";
-		}
-		else
-		{
-			$sql = "INSERT INTO users SET name='" . $this->username . "', pass='" . md5($this->unencrypted_password) . "', role='" . $this->role . "';";
+		} else {
+			$sql = "INSERT INTO users SET name='" . $this->sanitize($this->username) . "', pass='" . md5($this->sanitize($this->unencrypted_password)) . "', role='" . $this->sanitize($this->role) . "';";
 		}
 		
-		$db = new mysqli(DBHOST, DBUSER, DBPASS, DBNAME);
-		$result = $db->query($sql);
-		if ($result) 
-		{
+		$result = $this->db->query($sql);
+
+		if ($result) {
 			return true;
-		}
-		else
-		{
+		} else {
 			$this->error[] = "Failed to save user." . $sql;
 			return false;
 		}
 	}
 
-	public function delete()
-	{
-		$sql = "DELETE FROM users WHERE name='" . $this->username . "';";
-		$db = new mysqli(DBHOST, DBUSER, DBPASS, DBNAME);
-		$result = $db->query($sql);
-		if ($result)
-		{
+	public function delete() {
+		$sql = "DELETE FROM users WHERE name='" . $this->sanitize($this->username) . "';";
+		$result = $this->db->query($sql);
+		if ($result) {
 			return true;
-		}
-		else
-		{
+		} else {
 			return false;
 		}
 	}
